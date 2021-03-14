04d80f32def6ddd7a5a2317a69d901468c9cd606
Utkarsh Pant <utkarsh.pant@outlook.com>
2021-03-14 13:37:40 +0530

    Corrected unpublish link in Editor

--- src/Components/Editor/Editor.js
+++ src/Components/Editor/Editor.js
@@ -223,7 +223,7 @@ class Editor extends Component {
                                                         className="EditorButton"
                                                         onClick={(event) => {
                                                             event.preventDefault();
-                                                            axios.post(`https://localhost:4000/api/story/${this.state.isPublished ? "unpublish" : "publish"}/${this.state.storyId}`)
+                                                            axios.post(`https://telemetry-blog.herokuapp.com/api/story/${this.state.isPublished ? "unpublish" : "publish"}/${this.state.storyId}`)
                                                                 .then(response => {
                                                                     window.location.reload();
                                                                 })
