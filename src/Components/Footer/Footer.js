bb651cd5a7d66cca0ee931e215f62e0083c40063
Utkarsh Pant <utkarsh.pant@outlook.com>
2021-03-14 13:16:31 +0530

    Corrected footer link to github

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -10,7 +10,7 @@ const footer = () => {
             </Col>
             <Col md={4} sm={12} className="text-lg-center text-md-left text-sm-left FooterText">
                     <span>Made by Utkarsh Pant.</span>
-                    <a href="github.com/utkarshpant" className="FooterLink"><i class="fab fa-github"></i></a>
+                    <a href="https://github.com/utkarshpant" className="FooterLink"><i class="fab fa-github"></i></a>
                     <a href="https://twitter.com/utkarshpant15" className="FooterLink"><i class="fab fa-twitter"></i></a>
                     <a href="https://linkedin.com/in/utkarshpant" className="FooterLink"><i class="fab fa-linkedin"></i></a>
             </Col>
